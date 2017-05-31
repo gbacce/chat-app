004fa18eca48fea98d907960b7e78c29aa54da40
Guido Bacce <gbacce@MacBook-Pro.local>
2017-05-31 10:30:40 -0400

    add config.js

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ var fs = require("fs");
 // Include socket.io, which was installed by npm. It is not part of core.
 var socketio = require("socket.io");
 
+var usersArray = [];
+
 var server = http.createServer((req,res)=>{
 	console.log("Someone connected via HTTP!");
 
@@ -53,6 +55,16 @@ var server = http.createServer((req,res)=>{
 				res.end(data);
 			}			
 		});
+	} else if(req.url == '/config.js'){
+		fs.readFile('config.js', 'utf-8',(error,data)=>{
+			if(error) {
+				res.writeHead(500,{'content-type':'text/html'});
+				res.end('Internal Server Error');
+			} else{
+				res.writeHead(200,{'content-type':'application/javascript'});
+				res.end(data);
+			}			
+		});
 	} else{
 		
 	}
@@ -69,6 +81,7 @@ io.sockets.on('connect',(socket)=>{
 	socket.on('nameToServer',(name)=>{
 		console.log(name + " just joined.");
 		io.sockets.emit('newUser',name);
+		
 	});
 
 	socket.on('sendMessage',()=>{
@@ -78,6 +91,10 @@ io.sockets.on('connect',(socket)=>{
 	socket.on('messageToServer',(messageObj)=>{
 		io.sockets.emit('messageToClient', `<span>${messageObj.name}</span> &nbsp ${messageObj.newMessage}`);
 	});
+
+	socket.on('disconnect',(data)=>{
+		
+	})
 
 
 
